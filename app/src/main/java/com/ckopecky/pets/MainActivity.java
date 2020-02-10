6fd16ab74cfdb813626b00b6d96e573c7f0dd7f4
ckopecky <kopecky12112@gmail.com>
2020-02-10 10:57:30 -0800

    Added petnames

--- app/src/main/java/com/ckopecky/pets/MainActivity.java
+++ app/src/main/java/com/ckopecky/pets/MainActivity.java
@@ -1,14 +1,25 @@
+
 package com.ckopecky.pets;
+
+import android.app.Activity;
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    String[] mobileArray = {"Marshmallow", "Adagio", "Leonardo"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, mobileArray);
+
+        ListView listView = (ListView) findViewById(R.id.list_pets);
+        listView.setAdapter(adapter);
     }
 }
